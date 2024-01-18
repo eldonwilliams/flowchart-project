55d2ccdae99d1548d4fd405cd01af7801e9ec8f5
Eldon Williams <eldon.ew.com@gmail.com>
2024-01-18 11:44:59 -0600

    split contextMenu between cell and graph

--- src/CustomPopupMenuHandler.ts
+++ src/CustomPopupMenuHandler.ts
@@ -6,9 +6,30 @@ export default class CustomPopupMenuHandler extends PopupMenuHandler {
 
         // @ts-ignore
         this.factoryMethod = (handler: CustomPopupMenuHandler, cell: Cell, me: MouseEvent) => {
-            handler.addItem("Delete", null, function() {
-                graph.removeCells([cell], true);
-            });
+            if (cell != null)
+                this.cellContextMenu(handler, cell, me);
+            else
+                this.graphContextMenu(handler, me);
         }
+    }
+
+    cellContextMenu(handler: CustomPopupMenuHandler, cell: Cell, me: MouseEvent) {
+        const graph = this.graph;
+        this.addItem("Delete", null, function() {
+            graph.removeCells([cell]);
+        });
+    }
+
+    graphContextMenu(handler: CustomPopupMenuHandler, me: MouseEvent) {
+        const graph = this.graph;
+        this.addItem("Add Vertex", null, function() {
+            graph.insertVertex({
+                parent: graph.getDefaultParent(),
+                height: 20,
+                width: 20,
+                x: me.x,
+                y: me.y,
+            });
+        });
     }
 }
