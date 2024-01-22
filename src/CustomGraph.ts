22d7dcddf30990995aebff4106e4dcb035d2ba8c
Eldon Williams <eldon.ew.com@gmail.com>
2024-01-22 09:13:45 -0600

    add zoom

--- src/CustomGraph.ts
+++ src/CustomGraph.ts
@@ -1,4 +1,4 @@
-import { Cell, CellRenderer, Client, ConnectionHandler, GraphPluginConstructor, InternalMouseEvent } from "@maxgraph/core";
+import { CellRenderer, Client, EventObject, GestureEvent, GraphPluginConstructor, InternalEvent } from "@maxgraph/core";
 import { EDGESTYLE } from "@maxgraph/core/dist/util/Constants";
 import { Graph, defaultPlugins } from "@maxgraph/core/dist/view/Graph";
 import CellState from "@maxgraph/core/dist/view/cell/CellState";
@@ -6,7 +6,7 @@ import CustomPopupMenuHandler from "./CustomPopupMenuHandler";
 import CustomConnectionHandler from "./CustomConnectionHandler";
 import CustomCellRenderer from "./CustomCellRenderer";
 
-const defaultPluginsMap: {[key: string]: GraphPluginConstructor} = {};
+const defaultPluginsMap: { [key: string]: GraphPluginConstructor } = {};
 defaultPlugins.forEach(p => defaultPluginsMap[p.pluginId] = p);
 
 export default class CustomGraph extends Graph {
@@ -25,12 +25,14 @@ export default class CustomGraph extends Graph {
         this.setCellsEditable(true);
         this.setPanning(true);
 
-        container.addEventListener('wheel', (wv: WheelEvent) => {
-            wv.preventDefault();
-            this.panGraph(wv.deltaX + this.panDx, wv.deltaY + this.panDy);
-        }, {
-            passive: false
+        InternalEvent.addGestureListeners(container, null, () => {
+
         });
+
+        InternalEvent.addMouseWheelListener((event: WheelEvent) => {
+            if (!event.ctrlKey) return;
+            this.zoom(event.deltaY / 1000);
+        }, container);
 
         // delete selected vertex when delete or backspace is hit
         addEventListener('keydown', event => {
@@ -49,8 +51,8 @@ export default class CustomGraph extends Graph {
     // copied from a story book example, this allows constraints to work
     getAllConnectionConstraints = (terminal: CellState, source: boolean) => {
         if (terminal && terminal.cell) {
-            const geom = terminal.cell.geometry as {[a: string]: any};
-            const shape = terminal.shape as {[a: string]: any};
+            const geom = terminal.cell.geometry as { [a: string]: any };
+            const shape = terminal.shape as { [a: string]: any };
             if (shape.stencil) {
                 if (shape.stencil.constraints) {
                     return shape.stencil.constraints;
