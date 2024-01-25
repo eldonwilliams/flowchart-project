33fd1b06e6efe6960970616dbf38f42fe6ebedc2
Eldon Williams <eldon.ew.com@gmail.com>
2024-01-25 15:35:12 -0600

    Create ZoomHandler.ts

---
+++ src/plugins/ZoomHandler.ts
@@ -0,0 +1,32 @@
+import { GraphPlugin, InternalEvent } from "@maxgraph/core";
+import { Graph } from "@maxgraph/core/dist/view/Graph";
+
+/**
+ * This plugin handles zooming in/out of the graph using the "wheel" event.
+ */
+export default class ZoomHandler implements GraphPlugin {
+    static pluginId = "ZoomHandlerPlugin";
+
+    graph: Graph;
+
+    constructor (graph: Graph) {
+        this.graph = graph;
+        InternalEvent.addMouseWheelListener(
+            this.handleMouseWheelEvent,
+            graph.container
+        );
+    }
+
+    private handleMouseWheelEvent(event: WheelEvent) {
+        if (!event.ctrlKey) return;
+        this.graph.zoom(event.deltaY / 1000);
+    }
+
+    onDestroy() {
+        InternalEvent.removeListener(
+            this.graph.container,
+            "wheel",
+            this.handleMouseWheelEvent
+        );
+    }
+}
