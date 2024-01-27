bbbae326a3355b62a826be04d5d025ea8c96d3f6
Eldon Williams <eldon.ew.com@gmail.com>
2024-01-27 17:05:21 -0600

    register all shapes

--- src/plugins/CustomCellRenderer.ts
+++ src/plugins/CustomCellRenderer.ts
@@ -1,5 +1,15 @@
-import { CellRenderer, Client, EventObject, InternalEvent, InternalMouseEvent } from "@maxgraph/core";
+import { CellRenderer, Shape } from "@maxgraph/core";
+import Shapes from "../shapes/Shapes";
+
 
 export default class CustomCellRenderer extends CellRenderer {
-    
+    static registeredShapes = false;
+
+    constructor() {
+        super();
+        if (!CustomCellRenderer.registeredShapes) {
+            Shapes.forEach(v => CustomCellRenderer.registerShape(v.shapeKey, v as unknown as typeof Shape));
+            CustomCellRenderer.registeredShapes = true;
+        }
+    }
 }
