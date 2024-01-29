7576b396e88e75279831ae98acdd86e390680cb9
Eldon Williams <eldon.ew.com@gmail.com>
2024-01-29 09:01:13 -0600

    fixed, I think

--- src/plugins/ZoomHandler.ts
+++ src/plugins/ZoomHandler.ts
@@ -1,4 +1,4 @@
-import { Graph, GraphPlugin, InternalEvent } from "@maxgraph/core";
+import { Graph, GraphPlugin, InternalEvent, PanningHandler } from "@maxgraph/core";
 
 /**
  * This plugin handles zooming in/out of the graph using the "wheel" event.
@@ -7,25 +7,33 @@ export default class ZoomHandler implements GraphPlugin {
     static pluginId = "ZoomHandlerPlugin";
 
     graph: Graph;
+    panningHandler: PanningHandler;
 
     constructor (graph: Graph) {
         this.graph = graph;
+        this.panningHandler = graph.getPlugin(PanningHandler.pluginId) as PanningHandler;
         InternalEvent.addMouseWheelListener(
-            this.handleMouseWheelEvent,
+            this.handleMouseWheelEvent.bind(this),
             graph.container
         );
     }
 
     private handleMouseWheelEvent(event: WheelEvent) {
-        if (!event.ctrlKey) return;
-        this.graph.zoom(event.deltaY / 1000);
+        if (!event.ctrlKey) {
+            this.panningHandler.dx += event.deltaX;
+            this.panningHandler.dy += event.deltaY;
+            this.graph.panGraph(this.panningHandler.dx, this.panningHandler.dy);
+            event.preventDefault();
+            return;
+        }
+        this.graph.zoom(1 + event.deltaY / 500);
     }
 
     onDestroy() {
         InternalEvent.removeListener(
             this.graph.container,
             "wheel",
-            this.handleMouseWheelEvent
+            this.handleMouseWheelEvent.bind(this)
         );
     }
 }
