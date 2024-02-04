36257ecaf4a85e272fc85323a9465b22446a5014
Eldon Williams <eldon.ew.com@gmail.com>
2024-02-04 15:53:00 -0600

    Update Shapes.ts

--- src/shapes/Shapes.ts
+++ src/shapes/Shapes.ts
@@ -2,5 +2,6 @@ import { Shape } from "@maxgraph/core";
 import AttributeShape from "./AttributeShape";
 import EntityShape from "./EntityShape";
 import RelationShape from "./RelationShape";
+import DoubleAttributeShape from "./DoubleAttributeShape";
 
-export default [AttributeShape, EntityShape, RelationShape];
+export default [AttributeShape, EntityShape, RelationShape, DoubleAttributeShape];
