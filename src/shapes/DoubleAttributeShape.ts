bca8537eecc304265d92091589ab6f6c75e80a5d
Eldon Williams <eldon.ew.com@gmail.com>
2024-02-04 15:52:48 -0600

    Create DoubleAttributeShape.ts

---
+++ src/shapes/DoubleAttributeShape.ts
@@ -0,0 +1,15 @@
+import { CellState, DoubleEllipseShape, Geometry } from "@maxgraph/core";
+import CustomShapeInterface from "./CustomShapeInterface";
+import EllipseGeometryClass from "./geometry/EllipseGeometryClass";
+
+export default class DoubleAttributeShape extends DoubleEllipseShape implements CustomShapeInterface {
+    static shapeKey = "DoubleAttribute";
+    static friendlyName: string = "Double Attribute";
+    static geometryClass: typeof Geometry = EllipseGeometryClass;
+    static nonDoubleShape: string = "Attribute";
+    static isDoubleShape: boolean = true;
+    static defaultCellState: Partial<CellState> = {
+        width: 70,
+        height: 40
+    };
+}
