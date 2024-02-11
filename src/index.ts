23838873f6b6a2ec7144c2186509a556c86ada20
Eldon Williams <eldon.ew.com@gmail.com>
2024-02-10 23:17:06 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,5 +29,36 @@ loadButton.onclick = () => {
     deserializeGraph(graph, saveData);
 }
 
+const saveFileButton = document.createElement("button");
+saveFileButton.innerText = "Save File";
+saveFileButton.onclick = () => {
+    if (saveData === "") return;
+    const blob = new Blob([saveData], { type: "text/plain;charset=utf-8" });
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(blob);
+    a.download = "graph.dat";
+    a.click();
+}
+
+const fileInput = document.createElement("input");
+fileInput.type = "file";
+fileInput.onchange = (event) => {
+    const file = (event.target as HTMLInputElement).files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const contents = e.target.result as string;
+            console.log(contents)
+            deserializeGraph(graph, contents);
+        }
+        reader.readAsText(file);
+    }
+}
+
 topbar.appendChild(saveButton);
 topbar.appendChild(loadButton);
+topbar.appendChild(saveFileButton);
+topbar.appendChild(fileInput);
+
+// @ts-ignore
+window.graph = graph;
