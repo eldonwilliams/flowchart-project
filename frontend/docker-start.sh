06d3612e909aebcb13efcb62b77ccd364f9c08fb
Eldon Williams <eldon.ew.com@gmail.com>
2024-03-10 00:59:55 -0600

    docker compose watch

--- frontend/docker-start.sh
+++ frontend/docker-start.sh
@@ -3,8 +3,8 @@
 # This file handles starting the frontend
 
 if [ "$PROD" = "false" ]; then
-    npm install -g nodemon ts-node
-    npm run docker-dev
+    export DEV=true
+    npm run build -- --watch --outDir /usr/frontend/build --emptyOutDir
     exit 0
 elif [ "$PROD" = "true" ]; then
     bash ./build-docker.sh
