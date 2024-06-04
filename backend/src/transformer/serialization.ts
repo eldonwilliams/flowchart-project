008cd9197ed079d10074754a8117cdee6de7e960
Eldon Williams <eldon.ew.com@gmail.com>
2024-06-04 00:06:36 -0500

    complete rewrite of serializer, direct copy of autoer

--- backend/src/transformer/serialization.ts
+++ backend/src/transformer/serialization.ts
@@ -1,165 +1,148 @@
-import { findIndexById } from "./util";
+interface Attribute {
+    id: string;
+    multivalue: boolean;
+    label: string;
+    key: boolean;
+    composite: boolean;
+}
 
-type ServerGenericVertexRepresentation = {
-  id: string;
-  label: string;
-  dashed: boolean;
-  underlined: boolean;
-  double: boolean;
-  updated: number;
-  created: number;
-  shape: string;
-  edges: number[];
-};
+function isAttribute(vertex: Vertex): vertex is Attribute {
+    return vertex.hasOwnProperty('composite');
+}
 
-type ServerEntityVertexRepresentation = ServerGenericVertexRepresentation & {
-  shape: "Entity" | "Relation";
-  attributes: ServerGenericVertexRepresentation[];
-};
+interface Relationship {
+    id: string;
+    weak: boolean;
+    label: string;
+    attributes: Attribute[];
+    connects: number[];
+}
+
+function isRelationship(vertex: Vertex): vertex is Relationship {
+    return vertex.hasOwnProperty('connects');
+}
+
+interface Entity {
+    id: string;
+    weak: boolean;
+    label: string;
+    attributes: Attribute[];
+}
+
+function isEntity(vertex: Vertex): vertex is Entity {
+    return !vertex.hasOwnProperty('connects') && !vertex.hasOwnProperty('composite');
+}
+
+type Edge = [number, number];
+
+type Vertex = Entity | Relationship | Attribute;
+
+interface Graph {
+    entities: Entity[];
+    relationships: Relationship[];
+}
 
 /**
- * Verticies can either be Generic or Entity. This distinction is made because entities have all their attributes
- * encoded in the attributes key. If the shape != "Entity" then attributes will be undefined.
+ * Gets all verticies which are connected to a given vertex
  */
-export type ServerVertexRepresentation = ServerGenericVertexRepresentation | ServerEntityVertexRepresentation;
-
-export type ServerEdgeRepresentation = {
-  cardinality: string;
-  double: boolean;
-  updated: number;
-  created: number;
-  source: number;
-  target: number;
-};
-
-export type ServerERRespresentation = {
-  vertices: ServerVertexRepresentation[];
-  edges: ServerEdgeRepresentation[];
-};
-
-const defaultVertex: ServerVertexRepresentation = {
-  id: "NULL",
-  label: "NULL",
-  dashed: false,
-  underlined: false,
-  double: false,
-  updated: -1,
-  created: -1,
-  shape: "NULL",
-  edges: [],
-};
-
-const defaultEdge: ServerEdgeRepresentation = {
-  cardinality: "NULL",
-  double: false,
-  updated: -1,
-  created: -1,
-  source: -1,
-  target: -1,
-};
-
-// use the accumulator pattern because verticies on the frontend
-// use a children property, but we don't want to use that on the backend
-export function deserializeVertex(
-  data: string,
-  accum: ServerVertexRepresentation[] = []
-): void {
-  let { cellId, value, children } = JSON.parse(data);
-
-  let vertex = { ...defaultVertex, ...value, id: cellId }; // gotta love the spread operator
-
-  if (children && children.length > 0) {
-    children.forEach((child: string) => deserializeVertex(child, accum));
-    return; // If there is children, we should not serialize this one. (In a well formed input this should only be the root. Children are not used)
-  }
-
-  accum.push(vertex);
-  return vertex;
+function getConnectedVerticies(vertex: Vertex, verticies: Vertex[], edges: Edge): Vertex[] {
+    return [];
 }
 
-export function deserializeEdge(
-  data: string,
-  verts: ServerVertexRepresentation[],
-  cache: Map<string, number> | undefined = undefined
-): ServerEdgeRepresentation {
-  let { source, target, value } = JSON.parse(data);
-  let sourceIndex = findIndexById(source, verts, cache);
-  let targetIndex = findIndexById(target, verts, cache);
-
-  let edge = {
-    ...defaultEdge,
-    ...value,
-    source: sourceIndex,
-    target: targetIndex,
-  };
-
-  return edge;
+function populateAttributesProperty(verticies: Vertex[], edges: Edge): Vertex[] {
+    return [];
 }
 
-function insertAttributes(edges: ServerEdgeRepresentation[], verticies: ServerVertexRepresentation[]): void {
-  verticies.forEach((attributeVertex, i) => {
-    if (attributeVertex.shape !== "Attribute") {
-      return;
+function populateConnectsProperty(verticies: Vertex[], edges: Edge): Vertex[] {
+    return [];
+}
+
+/**
+ * Deserializes a vertex with the most information available
+ * Some properties (attributes, connects) require a list of all verticies to populate
+ * 
+ */
+function deserializeVertex(vertex: string): Vertex | undefined {
+    let { value, cellId: id } = JSON.parse(vertex);
+    let { label, dashed, underlined, double, shape } = value;
+
+    if (shape == "Attribute") {
+        return {
+            composite: dashed ?? false,
+            key: underlined ?? false,
+            label,
+            multivalue: double ?? false,
+            id,
+        } satisfies Attribute;
     }
-    const edgesWithThisVertex = edges.filter(e => e.source == i || e.target == i);
-    edgesWithThisVertex.forEach((edge) => {
-      const otherVertex = verticies[edge.source === i ? edge.target : edge.source];
-      if (otherVertex.shape !== "Entity" && otherVertex.shape !== "Relation") {
-        return;
-      }
-      if ((otherVertex as ServerEntityVertexRepresentation).attributes === undefined) {
-        // TODO: Find a better way to do this, multiple consecutive casts is bad practice.
-        (otherVertex as ServerEntityVertexRepresentation).attributes = [];
-      }
-      // If the other vertex is Entity or Relation we can push this attribute into its list of attributes
-      (otherVertex as ServerEntityVertexRepresentation).attributes.push(attributeVertex);
-    });
-  })
+
+    if (shape == "Entity") {
+        return {
+            attributes: [],
+            label,
+            weak: double ?? false,
+            id,
+        } satisfies Entity;
+    }
+
+    if (shape == "Relation") {
+        return {
+            label,
+            attributes: [],
+            connects: [],
+            weak: double ?? false,
+            id,
+        } satisfies Relationship;
+    }
 }
 
-/**
- * Returns ne
- * @param edges 
- * @param verts 
- * @returns 
- */
-function makeEdges(edgesData: string[], verts: ServerVertexRepresentation[]): ServerEdgeRepresentation[] {
-  let cache: Map<string, number> = new Map<string, number>();
-  return edgesData.map((edge) => deserializeEdge(edge, verts, cache));
+function getEdgesBetweenSets(a: Vertex[], b: Vertex[], edges: string[]): Edge[] {
+    return edges.map((e: string) => {
+        const { source, target } = JSON.parse(e);
+
+        return ([
+            a.findIndex((v) => v.id === source || v.id === target),
+            b.findIndex((v) => v.id === source || v.id === target),
+        ]);
+    }).filter((v: number[]) => v.indexOf(-1) === -1 && v.length === 2).map((v: number[]) => v as Edge);
 }
 
-function mapVertexToEdgeIndicies(edges: ServerEdgeRepresentation[], verts: ServerVertexRepresentation[]) {
-  return verts.map((v, i) => ({
-    ...v,
-    edges: edges
-      .map((e, i) => ({ ...e, idx: i, }))
-      .filter(e => e.source === i || e.target === i)
-      .map((e) => e.idx),
-  }))
+function insertAttributesToSet<T>(set: T[], attributes: Attribute[], edges: Edge[]): T[] {
+    return set.map((v, i) => ({
+        ...v,
+        attributes: attributes.filter((_, j) => edges.findIndex((edge) => edge[0] === i && edge[1] === j) != -1),
+    }));
 }
 
-export function deserializeGraph(data: string): ServerERRespresentation | undefined {
-  let parsed = JSON.parse(data);
-  let vertices: ServerVertexRepresentation[] = [];
-  let cache = new Map<string, number>(); // use a memo for index to id lookups
-  let rootChildrenVerticies: string[] = JSON.parse(parsed.root).children;
-  rootChildrenVerticies.forEach(v => deserializeVertex(v, vertices));
+export function deserializeGraph(data: string): Graph {
+    let { root, edges } = JSON.parse(data);
 
-  let edges: ServerEdgeRepresentation[] = makeEdges(parsed.edges, vertices)
-  if (rootChildrenVerticies === undefined || rootChildrenVerticies.length == 0) {
-    return undefined;
-  }
+    let verticies = JSON.parse(root).children;
+    verticies = verticies.map(deserializeVertex)
 
-  
-  insertAttributes(edges, vertices);
-  // remove edges where either souce or target is a Attribute
-  edges = edges.filter(e =>
-    !(vertices[e.source].shape === "Attribute" || vertices[e.target].shape === "Attribute")
-  );
-  vertices = vertices.filter(v => v.shape !== "Attribute");
-  edges = makeEdges(parsed.edges, vertices).filter((edge) => edge.source !== -1 && edge.target !== -1);
-  vertices = mapVertexToEdgeIndicies(edges, vertices);
-  // filter attribute verts
+    let attributes: Attribute[] = verticies.filter(isAttribute);
+    let entities: Entity[] = verticies.filter(isEntity);
+    let relationships: Relationship[] = verticies.filter(isRelationship);
 
-  return { vertices, edges };
+    /**
+     * Array of edges of entity->relationship connections.
+     */
+    let entityRelationshipEdges: Edge[] = getEdgesBetweenSets(entities, relationships, edges);
+
+    relationships = relationships.map((r, i) => ({
+        ...r,
+        connects: entityRelationshipEdges
+            .filter(e => e[1] === i)
+            .map(edge => edge[0])
+    }));
+
+    let entitiesToAttributeEdges: Edge[] = getEdgesBetweenSets(entities, attributes, edges);
+    let relationshipToAttributeEdges: Edge[] = getEdgesBetweenSets(relationships, attributes, edges);
+    entities = insertAttributesToSet(entities, attributes, entitiesToAttributeEdges);
+    relationships = insertAttributesToSet(relationships, attributes, relationshipToAttributeEdges);
+
+    return {
+        entities,
+        relationships
+    }
 }
